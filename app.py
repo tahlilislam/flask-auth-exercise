64ab1a8390271c1a117252748d6b7a5194b164b9
tahlilislam <tahlilislam@gmail.com>
2023-12-09 18:43:55 -0800

    starter code

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User
+from forms import RegisterForm, LoginForm
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///hashing_login"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "abc123"
+
+connect_db(app)
+db.create_all()
+
+toolbar = DebugToolbarExtension(app)
+
